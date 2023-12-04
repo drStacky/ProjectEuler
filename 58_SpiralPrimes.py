84f55c1e0e4215da0d2d737d4b521b52c339652b
Matt Stackpole <matt.stackpole@maxar.com>
2023-12-04 13:07:34 -0700

    Use euler.is_prime (faster)

--- 58_SpiralPrimes.py
+++ 58_SpiralPrimes.py
@@ -27,14 +27,6 @@ from datetime import datetime
 from euler import is_prime
 
 
-def isPrime(n):
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-
-    return True
-
-
 if __name__ == '__main__':
     start = datetime.now()
 
